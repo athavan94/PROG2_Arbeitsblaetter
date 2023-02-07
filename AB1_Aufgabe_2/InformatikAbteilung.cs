4d082e02c6dfa1eefd4d6d74f9909f29172c640d
athavan94 <athavan94@gmail.com>
2023-02-07 20:12:32 +0100

    Aufgabe 2 Vererbung und Aufgabe 1 Anpassungen

---
+++ AB1_Aufgabe_2/InformatikAbteilung.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2_Arbeitsblaetter.AB1_Aufgabe_2
+{
+    abstract class InformatikAbteilung
+    {
+        public string Name { get; set; }
+        public string Rolle { get; set; }
+        private string Firma { get; set; } //Verstehe nicht für was diese private Methode gut sein soll.
+
+        public InformatikAbteilung()
+        {
+            Firma = "FIVE Informatik AG";
+            Console.WriteLine(Firma);
+        }
+
+        /// <summary>
+        /// Diese Methode muss überschrieben werden.
+        /// </summary>
+        public abstract void MeineAufgaben();
+
+        /// <summary>
+        /// Diese Methode kann bei der Vererbung überschreiben werden.
+        /// </summary>
+        public virtual void Vorstellen()
+        {
+            Console.WriteLine("Mein Name ist " + Name + ".");
+            Console.WriteLine("Ich bin ein " + Rolle + ".");
+        }
+    }
+
+    class TeamLeiter : InformatikAbteilung
+    {
+        public TeamLeiter()
+        {
+            this.Rolle = "Team Leiter";
+        }
+
+        public override void Vorstellen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bitte stellt euch alle kurz vor.");
+            Console.WriteLine();
+            base.Vorstellen();
+        }
+
+        public override void MeineAufgaben()
+        {
+            Console.WriteLine("Ich führe das Team und informiere über Neuigkeiten.");
+        }
+    }
+
+    class ProductOwner : InformatikAbteilung
+    {
+        public ProductOwner()
+        {
+            this.Rolle = "ProductOwner";
+        }
+
+        public override void MeineAufgaben()
+        {
+            Console.WriteLine("Ich bin verantwortlich für das Produkt und entscheide was entwickelt werden soll.");
+        }
+    }
+
+    class Softwareentwickler : InformatikAbteilung
+    {
+        public Softwareentwickler()
+        {
+            this.Rolle = "Softwareentwickler";
+        }
+
+        public override void MeineAufgaben()
+        {
+            Console.WriteLine("Ich bin für die Entwicklung des Produktes zuständig.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using PROG2_Arbeitsblaetter.AB1_Aufgabe_2;
 
 namespace PROG2_Arbeitsblaetter
 {
@@ -6,7 +8,39 @@ namespace PROG2_Arbeitsblaetter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // AB1 Aufgabe 1
+
+            /*
+            Console.WriteLine(Ueberladung.Plus(1,2)); //3
+            Console.WriteLine(Ueberladung.Plus(1,2,3)); //6
+            Console.WriteLine(Ueberladung.Plus(1,2,3,4)); //10
+            */
+
+            // AB1 Aufgabe 2
+
+            /*
+            TeamLeiter teamLeiter = new TeamLeiter();
+            ProductOwner productOwner = new ProductOwner();
+            Softwareentwickler softwareentwickler = new Softwareentwickler();
+
+            teamLeiter.Name = "Marco Müller";
+            productOwner.Name = "Nicola Schmid";
+            softwareentwickler.Name = "Marcel Zimmermann";
+
+            List<InformatikAbteilung> liste = new List<InformatikAbteilung>();
+
+            liste.Add(teamLeiter);
+            liste.Add(productOwner);
+            liste.Add(softwareentwickler);
+
+            foreach(InformatikAbteilung informatikAbteilung in liste)
+            {
+                informatikAbteilung.Vorstellen();
+                informatikAbteilung.MeineAufgaben();
+
+                Console.WriteLine();
+            }
+            */
         }
     }
 }
