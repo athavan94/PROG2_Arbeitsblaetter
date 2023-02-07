885e68f8df435c9ab53f2fd91ddf03939021507c
athavan94 <athavan94@gmail.com>
2023-02-07 17:09:15 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROG2_Arbeitsblaetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
