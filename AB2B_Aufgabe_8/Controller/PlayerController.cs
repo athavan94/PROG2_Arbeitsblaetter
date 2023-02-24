25d5a6773ac45f68c3d95906bbc4d9ce25fa28ab
athavan94 <athavan94@gmail.com>
2023-02-24 13:35:50 +0100

    AB2B Aufgabe 8 Update Funktion für Spieler

--- AB2B_Aufgabe_8/Controller/PlayerController.cs
+++ AB2B_Aufgabe_8/Controller/PlayerController.cs
@@ -100,5 +100,66 @@ namespace PROG2_Arbeitsblaetter.AB2B_Aufgabe_8.Controller
             }
             Console.WriteLine();
         }
+
+        public void UpdatePlayer()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Geben Sie den Namen des zu bearbeitenden Spielers ein:");
+            string name = Console.ReadLine();
+
+            using (Context context = new Context())
+            {
+                Player player = context.FindPlayerByName(name);
+                if (player != null)
+                {
+                    Console.WriteLine("Geben Sie den neuen Namen des Spielers ein (oder drücken Sie Enter, um den aktuellen Namen beizubehalten):");
+                    string playerName = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(playerName))
+                    {
+                        player.Name = playerName;
+                    }
+
+                    Console.WriteLine("Geben Sie den neuen Vornamen des Spielers ein (oder drücken Sie Enter, um den aktuellen Vornamen beizubehalten):");
+                    string vorname = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(vorname))
+                    {
+                        player.Vorname = vorname;
+                    }
+
+                    Console.WriteLine("Geben Sie die neue Position des Spielers ein (oder drücken Sie Enter, um die aktuelle Position beizubehalten):");
+                    string position = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(position))
+                    {
+                        player.Position = position;
+                    }
+
+                    Console.WriteLine("Geben Sie die neue Nationalität des Spielers ein (oder drücken Sie Enter, um die aktuelle Nationalität beizubehalten):");
+                    string nationalität = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(nationalität))
+                    {
+                        player.Nationalität = nationalität;
+                    }
+
+                    Console.WriteLine("Geben Sie den Namen des neuen Teams ein (oder drücken Sie Enter, um das aktuelle Team beizubehalten):");
+                    string teamName = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(teamName))
+                    {
+                        Team team = context.FindTeamByName(teamName);
+                        if (team != null)
+                        {
+                            player.Team = team;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Team mit dem Namen {0} wurde nicht gefunden.", team.Name);
+                        }
+                    }
+
+
+                    context.SaveChanges();
+                }
+            }
+            Console.WriteLine();
+        }
     }
 }
